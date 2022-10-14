79dd386cda00bb16e3c10df2e7708030461e7305
yorocomv <ishihara@gyokuroen.co.jp>
2022-10-14 17:17:25 +0900

    yarn add react-hook-form

--- frontend/src/routes/search-customer.jsx
+++ frontend/src/routes/search-customer.jsx
@@ -1,31 +1,39 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Flex, HStack, Input, Button, VStack, Text } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
 
 const axiosInst = axios.create({
     baseURL: 'http://localhost:3001',
 });
 
 const SearchCustomer = () => {
+    const [searchName, setsearchName] = useState(null);
     const [customers, setCustomers] = useState([]);
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = d => setsearchName(d.search_name);
+
     useEffect(() => {
         const getCustomers = async () => {
             try {
-                const res = await axiosInst.get('/search?name=伊藤忠食品');
+                const res = await axiosInst.get(`/search?name=${searchName}`);
                 setCustomers(res.data);
             } catch (err) {
                 console.error(err);
             }
         };
         getCustomers();
-    }, []);
+    }, [searchName]);
 
     return (
         <>
             <Flex width="100%" align="center" justify="center" padding="3px" bg="gray.700">
                 <HStack>
-                    <Input bg="white" />
-                    <Button>検索</Button>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <Input bg="white" {...register("search_name")} />
+                        <Button type="submit">検索</Button>
+                    </form>
                 </HStack>
             </Flex>
             <VStack padding={4}>
