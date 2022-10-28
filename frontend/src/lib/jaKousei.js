a8cb9f4dbd07bd3730c6185d3d54aa6824e2d988
yorocomv <ishihara@gyokuroen.co.jp>
2022-10-28 12:30:51 +0900

    名称表記揺れ吸収を更新

--- frontend/src/lib/jaKousei.js
+++ frontend/src/lib/jaKousei.js
@@ -26,7 +26,7 @@ const jaKousei = (orgName) => {
     name = name.replace( /支店/g, "店");
     name = name.replace( /センタ/g, "C");
     name = name.replace( /物流ステション/g, "DS");
-    name = name.replace( /流通C|物流C|配送C|商品C|食品C/g, "C");
+    name = name.replace( /流通C|物流C|配送C|共同配送C|共配C|商品C|食品C/g, "C");
     name = name.replace( /常温C|冷温C|冷蔵冷凍C|冷凍冷蔵C|冷蔵C|冷凍C|ドライC/g, "C");
     name = name.replace( /RDC|FDC|SDC|DC|SC/g, "C");
     name = name.replace( /オ茶ノ|茶ノ|C&C/g, "");
