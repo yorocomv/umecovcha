b16f886c2f19d412ca03f62948068ce387a42289
yorocomv <ishihara@gyokuroen.co.jp>
2023-03-10 12:52:16 +0900

    メモ登録機能実装中

--- frontend/src/routes/record-note.jsx
+++ frontend/src/routes/record-note.jsx
@@ -1,18 +1,41 @@
-import { useState } from 'react';
-import { Container, FormControl, FormLabel, Textarea, VStack } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { Container, FormControl, FormLabel, Text, Textarea, VStack } from '@chakra-ui/react';
 import cs from '../addStyles.module.css';
+import { useParams } from 'react-router-dom';
+import { axiosInst } from './_axios-instance';
 
 const RecordNote = () => {
-    const [noteLength, setNoteLength] = useState(0);
+    const maxLength = 255;
+    const [noteLength, setNoteLength] = useState(maxLength);
+    const [customer, setCustomer] = useState({});
+    const [notes, setNotes] = useState([]);
 
-    const handleChange = e => {};
+    const { id } = useParams();
+
+    useEffect(() => {
+        const getCustomer = async () => {
+            try {
+                const resCustomer = await axiosInst.get(`/customers/${id}`);
+                setCustomer(resCustomer.data[0]);
+                const resNotes = await axiosInst.get(`/notes/${id}`);
+                setNotes(resNotes.data);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+        getCustomer();
+    }, []);
+
+    const handleChange = e => { setNoteLength(maxLength - e.target.value.length) };
     return (
         <VStack p={4}>
+            <Text fontSize='lg'>{customer.name1}</Text>
+            <Text fontSize='lg'>{customer.name2}</Text>
             <Container width='4xl' p={4} borderRadius={4} className={cs.lightSpot}>
                 <form>
                     <FormControl>
-                        <FormLabel htmlFor='note'>留意事項</FormLabel>
-                        <Textarea id='note' autoFocus={true} height='3xs' />
+                        <FormLabel htmlFor='note'>留意事項！ (残り {noteLength} 文字)</FormLabel>
+                        <Textarea onChange={handleChange} id='note' autoFocus={true} height='3xs' />
                     </FormControl>
                 </form>
             </Container>
